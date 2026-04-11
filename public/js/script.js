3d88620f1df4231c77af6769fba61a1878548c98
Julio Milan <125148176+milanen@users.noreply.github.com>
2026-04-11 15:59:34 -0300

    Implement calculator with HTML, JS and CSS

---
+++ public/js/script.js
@@ -0,0 +1,23 @@
+const display = document.querySelector("input");
+const buttons = document.querySelectorAll(".buttons button");
+const clearBttn = document.querySelector("#clear-bttn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+
+    if (value === "=") {
+      try {
+        display.value = eval(display.value);
+      } catch {
+        display.value = "Error";
+      }
+    } else {
+      display.value += value;
+    }
+  });
+});
+
+clearBttn.addEventListener("click", () => {
+  display.value = "";
+});
